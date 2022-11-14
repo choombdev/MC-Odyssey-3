5d16f7ea0cc77c6ef96085f13eb284e0baa51b8e
Choomb <Yahiadalash@gmail.com>
2022-11-14 15:59:52 +0200

    Create InsolatorRecipes.js

---
+++ kubejs/server_scripts/Odyssey 3/Mods/Thermal/InsolatorRecipes.js
@@ -0,0 +1,5 @@
+// This script was made by choombdev for Odyssey 3, reusing it without explicit permission is not allowed.
+onEvent('recipes', event => {
+
+event.recipes.thermal.insolator('mysticalagriculture:inferium_seeds', 'mysticalagriculture:inferium_essence').water(100)})
+
