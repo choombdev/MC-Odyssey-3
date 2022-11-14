572c6cd85325365f982dfc1ec5731d7f3b826daf
Choomb <Yahiadalash@gmail.com>
2022-11-14 15:41:45 +0200

    Gobber and Botania integration.

---
+++ kubejs/server_scripts/Odyssey 3/Mods/Botania/GobberIntegration.js
@@ -0,0 +1,9 @@
+// This script was made by choombdev for Odyssey 3, reusing it without explicit permission is not allowed.
+onEvent('recipes', event => {
+  event.recipes.botania.terra_plate('gobber2:gobber2_globette', ["thermal:netherite_gear"], 50000)
+  event.recipes.botania.terra_plate('gobber2:gobber2_globette_nether', ['gobber2:gobber2_globette'], 25000)
+  event.recipes.botania.terra_plate('gobber2:gobber2_globette_end', ['gobber2:gobber2_globette'], 25000)
+
+
+})
+
