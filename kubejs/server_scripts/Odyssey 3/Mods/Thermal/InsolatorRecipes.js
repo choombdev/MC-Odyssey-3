c835a71a05ac75e54b0f9c890c923173ceb71667
Choomb <Yahiadalash@gmail.com>
2022-11-20 15:48:30 +0200

    Fixed Inferium Seed recipe in Phytogenic Insulators

--- kubejs/server_scripts/Odyssey 3/Mods/Thermal/InsolatorRecipes.js
+++ kubejs/server_scripts/Odyssey 3/Mods/Thermal/InsolatorRecipes.js
@@ -1,5 +1,5 @@
 // This script was made by choombdev for Odyssey 3, reusing it without explicit permission is not allowed.
 onEvent('recipes', event => {
 
-event.recipes.thermal.insolator('mysticalagriculture:inferium_seeds', 'mysticalagriculture:inferium_essence').water(100)})
+event.recipes.thermal.insolator('mysticalagriculture:inferium_essence','mysticalagriculture:inferium_seeds').water(100)})
 
