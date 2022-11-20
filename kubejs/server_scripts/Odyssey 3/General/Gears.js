0d62117f37e3fbfa903401be5599ea8c6619defe
Choomb <Yahiadalash@gmail.com>
2022-11-20 16:43:26 +0200

    Fixed some gears having an easier recipe, making the Multiservo Press useless.

---
+++ kubejs/server_scripts/Odyssey 3/General/Gears.js
@@ -0,0 +1,8 @@
+// This script was made by Choomb for Odyssey 3, reusing it without explicit permission is not allowed.
+onEvent('recipes', event => {
+// Removing
+event.remove({id: 'industrialforegoing:iron_gear'})
+event.remove({id: 'industrialforegoing:diamond_gear'})
+event.remove({id: 'industrialforegoing:gold_gear'})
+
+})
