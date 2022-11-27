d2674d3c53157290c42182632c0b32802258bd77
Choomb <Yahiadalash@gmail.com>
2022-11-27 02:35:28 +0200

    Fixed Diamond Plates having no machine recipes.

--- kubejs/server_scripts/Odyssey 3/General/Plates.js
+++ kubejs/server_scripts/Odyssey 3/General/Plates.js
@@ -12,4 +12,9 @@ mcmaterials.forEach(material => {
   event.remove({id: 'immersiveengineering:crafting/plate_'+material+'_hammering'})
   event.shapeless('alltheores:'+material+'_plate', [Item.of('immersiveengineering:hammer').ignoreNBT(),'minecraft:'+material+'_ingot','minecraft:'+material+'_ingot']).damageIngredient(Item.of('immersiveengineering:hammer'))
 })
+
+// Adding missing Diamond Plate Processing recipe
+event.recipes.immersiveengineeringMetalPress('alltheores:diamond_plate', 'minecraft:diamond', 'immersiveengineering:mold_plate')
+event.recipes.thermal.press('alltheores:diamond_plate', 'minecraft:diamond')
+event.recipes.createPressing('alltheores:diamond_plate', 'minecraft:diamond')
 })
