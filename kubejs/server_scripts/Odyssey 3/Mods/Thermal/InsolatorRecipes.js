d18eacc423da66781ee6f8a27b0befaf8e7bae50
Choomb <Yahiadalash@gmail.com>
2022-12-19 17:04:44 +0200

    Added MNA recipes to the Phytogenic Insolator from Thermal

--- kubejs/server_scripts/Odyssey 3/Mods/Thermal/InsolatorRecipes.js
+++ kubejs/server_scripts/Odyssey 3/Mods/Thermal/InsolatorRecipes.js
@@ -1,5 +1,14 @@
 // This script was made by choombdev for Odyssey 3, reusing it without explicit permission is not allowed.
 onEvent('recipes', event => {
 
-event.recipes.thermal.insolator('mysticalagriculture:inferium_essence','mysticalagriculture:inferium_seeds').water(100)})
+event.recipes.thermal.insolator('mysticalagriculture:inferium_essence','mysticalagriculture:inferium_seeds').water(100)
+
+// Missing MNA Recipes
+event.recipes.thermal.insolator('mna:aum','mna:aum').water(100)
+event.recipes.thermal.insolator('mna:cerublossom','mna:cerublossom').water(100)
+event.recipes.thermal.insolator('mna:tarma_root','mna:tarma_root').water(100)
+event.recipes.thermal.insolator('mna:wakebloom','mna:wakebloom').water(100)
+event.recipes.thermal.insolator('mna:desert_nova','mna:desert_nova').water(100)
+
+})
 
