8e0d45d9bd4a577faea119b01ebe72c07dcb6085
Choomb <Yahiadalash@gmail.com>
2023-02-10 02:39:43 +0200

    Fixed unintended recipe and cleaned up the file a bit more.

--- kubejs/server_scripts/Odyssey 3/Mods/RedstoneArsenal/Recipe Changing.js
+++ kubejs/server_scripts/Odyssey 3/Mods/RedstoneArsenal/Recipe Changing.js
@@ -1,8 +1,13 @@
 // This script was made by choombdev for Odyssey 3, reusing it without explicit permission is not allowed.
 onEvent('recipes', event => {
-    
+    // Removing all Old Recipes
+        // Crafting
+        event.remove({output: 'redstone_arsenal:flux_dust'})
+        event.remove({output:'redstone_arsenal:flux_gem'})
+        // Induction
+        event.remove({id: 'redstone_arsenal:materials/flux_ingot_fire_charge_from_dust'})
+        event.remove({id:'thermal:compat/redstone_arsenal/smelter_rsa_alloy_flux'})
    // Flux Dust
-    event.remove({output: 'redstone_arsenal:flux_dust'})
     event.shaped('redstone_arsenal:flux_dust', [
         'FEF',
         'NCN',
@@ -13,10 +18,8 @@ onEvent('recipes', event => {
         N: 'thermal:netherite_dust',
         C: 'mekanism:basic_control_circuit'
     })
-    // Flux Ingots
-    event.remove({id: 'redstone_arsenal:materials/flux_ingot_fire_charge_from_dust'})
+
     // Flux Gem
-    event.remove({output:'redstone_arsenal:flux_gem'})
     event.shaped('redstone_arsenal:flux_gem', [
         'D D',
         ' G ',
