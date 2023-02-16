96461b59d6183ee4ad154801b33922229d8d15e6
Choomb <Yahiadalash@gmail.com>
2023-02-16 18:06:53 +0200

    Removed Duplicate Rod recipe

---
+++ kubejs/server_scripts/Odyssey 3/General/Rods.js
@@ -0,0 +1,6 @@
+// This script was made by Choomb for Odyssey 3, reusing it without explicit permission is not allowed.
+onEvent('recipes', event => {
+// Removing
+event.remove({id:'immersiveengineering:crafting/stick_steel'})
+
+})
