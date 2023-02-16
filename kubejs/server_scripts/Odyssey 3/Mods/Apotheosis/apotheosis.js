4c8c96b8c9194f470525930c2d345670d9d79b50
Choomb <Yahiadalash@gmail.com>
2023-02-16 18:07:03 +0200

    Added a recipe to the Apotheosis Boss Summoner

---
+++ kubejs/server_scripts/Odyssey 3/Mods/Apotheosis/apotheosis.js
@@ -0,0 +1,17 @@
+// This script was made by choombdev for Odyssey 3, reusing it without explicit permission is not allowed.
+onEvent('recipes', event => {
+
+  // Boss Spawner
+  event.shaped('apotheosis:boss_summoner',
+  [
+     'GDG',
+     'GXG',
+     'GDG'
+  ],{
+      G: 'ars_nouveau:source_gem',
+      D: 'apotheosis:gem_dust',
+      X: 'ars_nouveau:ritual_wilden_summon'
+  }) 
+ 
+})
+
