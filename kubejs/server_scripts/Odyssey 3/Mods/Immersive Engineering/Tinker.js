ae128d642c17ff5c901eb811a6936b41df3f9c33
Choomb <Yahiadalash@gmail.com>
2023-04-14 05:46:41 +0200

    Added a new recipe to the Arc Furnace.

---
+++ kubejs/server_scripts/Odyssey 3/Mods/Immersive Engineering/Tinker.js
@@ -0,0 +1,4 @@
+// This script was made by choombdev for Odyssey 3, reusing it without explicit permission is not allowed.
+onEvent('recipes', event => {
+event.recipes.immersiveengineeringArcFurnace('tinkers_reforged:gausum_ingot', 'tinkers_reforged:blazing_copper_ingot', ['3x minecraft:lapis_block', '3x minecraft:netherite_scrap', ])
+})
