4507ec9ee1b2d988d587c664da6b347cf2aa1a33
Choomb <Yahiadalash@gmail.com>
2023-04-14 05:46:54 +0200

    Fixed an issue with Ether recipes not working.

--- kubejs/server_scripts/Odyssey 3/Mods/Industrial Foregoing/Recipe Changing.js
+++ kubejs/server_scripts/Odyssey 3/Mods/Industrial Foregoing/Recipe Changing.js
@@ -1,9 +1,9 @@
 // This script was made by choombdev for Odyssey 3, reusing it without explicit permission is not allowed.
 onEvent('recipes', event => {
 //Ether Gas
-event.recipes.createMixing(Fluid.of("industrialforegoing:ether_gas",100), [
+event.recipes.createMixing(Fluid.of("industrialforegoing:ether_gas",75), [
   'forbidden_arcanus:dark_nether_star',
-  Fluid.of("mekanism:steam", 1500)
+  Fluid.of("mekanism:steam", 1000)
 ]).superheated()
 })
 
