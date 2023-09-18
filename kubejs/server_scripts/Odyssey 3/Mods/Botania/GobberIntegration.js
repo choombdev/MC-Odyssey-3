0bf30c4d3158e6511bcb5c1ae4693491995363fb
Choomb <Yahiadalash@gmail.com>
2023-09-19 02:35:36 +0300

    Fixing Gobber Progression

--- kubejs/server_scripts/Odyssey 3/Mods/Botania/GobberIntegration.js
+++ kubejs/server_scripts/Odyssey 3/Mods/Botania/GobberIntegration.js
@@ -2,7 +2,7 @@
 onEvent('recipes', event => {
   event.recipes.botania.terra_plate('gobber2:gobber2_globette', ["thermal:netherite_gear"], 50000)
   event.recipes.botania.terra_plate('gobber2:gobber2_globette_nether', ['gobber2:gobber2_globette'], 25000)
-  event.recipes.botania.terra_plate('gobber2:gobber2_globette_end', ['gobber2:gobber2_globette'], 25000)
+  event.recipes.botania.terra_plate('gobber2:gobber2_globette_end', ['gobber2:gobber2_globette_nether'], 25000)
 
 
 })
